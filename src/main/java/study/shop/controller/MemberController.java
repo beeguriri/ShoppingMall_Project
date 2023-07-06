04ab3b52448beb4d53544a73158437f8e274a680
beeguriri <kkamboting@gmail.com>
2023-07-06 16:11:01 +0900

    회원정보 수정반영 되는거 확인

--- src/main/java/study/shop/controller/MemberController.java
+++ src/main/java/study/shop/controller/MemberController.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import study.shop.dto.MemberFormDto;
 import study.shop.dto.MemberUpdateFormDto;
 import study.shop.entity.Member;
-import study.shop.repository.MemberRepository;
 import study.shop.service.MemberService;
 
 import javax.validation.Valid;
@@ -73,16 +73,22 @@ public class MemberController {
     }
 
     @PostMapping("/update")
-    public String update(@Valid MemberUpdateFormDto memberUpdateFormDto, BindingResult bindingResult) {
+    public String update(@ModelAttribute("updateMember") @Valid MemberUpdateFormDto memberUpdateFormDto, BindingResult bindingResult, Model model, Principal principal) {
 
-        // 입력값에 오류 있으면 bindingResult 에 담아서 form 으로 이동
-        if(bindingResult.hasErrors()) {
-            log.info("error={}", bindingResult.getFieldError());
-            return "members/memberUpdateForm";
-        }
 
-        memberService.updateMember(memberUpdateFormDto);
-
+//        // 입력값에 오류 있으면 bindingResult 에 담아서 form 으로 이동
+//        if(bindingResult.hasErrors()) {
+//            log.info("error={}", bindingResult.getFieldError());
+//            return "members/memberUpdateForm";
+//        }
+//
+//        try {
+            memberService.updateMember(memberUpdateFormDto);
+//        } catch (IllegalStateException e) {
+//            log.info("error={}", e.getMessage());
+//            model.addAttribute("errorMessage", e.getMessage());
+//            return "members/memberUpdateForm";
+//        }
         return "redirect:/";
     }
 }
