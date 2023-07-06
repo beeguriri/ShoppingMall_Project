1a167b7e46d4db3f2441a302f4f8519f212efd06
beeguriri <kkamboting@gmail.com>
2023-07-06 10:55:46 +0900

    회원가입 폼 입력값 검증

--- src/main/java/study/shop/dto/MemberFormDto.java
+++ src/main/java/study/shop/dto/MemberFormDto.java
@@ -1,23 +1,28 @@
 package study.shop.dto;
 
 import lombok.Data;
-import study.shop.entity.constant.Role;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class MemberFormDto {
 
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     private String userid;
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String nickName;
 
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "올바른 이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+//    @Length(min = 8, max = 16, message = "비밀번호는 8자이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private String address;
 
 }
