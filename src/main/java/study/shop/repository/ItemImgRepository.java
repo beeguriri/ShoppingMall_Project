03e54375330057482c2e7e6406a8c88e30d11be1
beeguriri <kkamboting@gmail.com>
2023-07-09 16:49:54 +0900

    아이템 이미지 정렬 메서드 추가

--- src/main/java/study/shop/repository/ItemImgRepository.java
+++ src/main/java/study/shop/repository/ItemImgRepository.java
@@ -3,5 +3,9 @@ package study.shop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.shop.entity.ItemImg;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
