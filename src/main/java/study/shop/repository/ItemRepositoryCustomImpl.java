cdd4737f566b023d77a45bcd189b3ea235854f64
beeguriri <kkamboting@gmail.com>
2023-07-10 15:14:56 +0900

    EntityManager 의존성주입 확인

--- src/main/java/study/shop/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/study/shop/repository/ItemRepositoryCustomImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -23,6 +24,11 @@ import static study.shop.entity.QItem.item;
 public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
+
+    @Autowired
+    public ItemRepositoryCustomImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     @Override
     public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
