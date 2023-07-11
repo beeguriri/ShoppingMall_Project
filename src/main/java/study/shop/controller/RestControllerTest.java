6ab8acdc23cbc786252fdafbeae014ccf1b9979f
beeguriri <kkamboting@gmail.com>
2023-07-11 17:40:40 +0900

    페이징 확인 위한 restcontroller 테스트

--- src/main/java/study/shop/controller/RestControllerTest.java
+++ src/main/java/study/shop/controller/RestControllerTest.java
@@ -4,21 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.shop.dto.ItemSearchDto;
+import study.shop.dto.OrderHistoryDto;
 import study.shop.entity.Item;
 import study.shop.service.ItemService;
+import study.shop.service.OrderService;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-public class ItemControllerTest {
+public class RestControllerTest {
 
     private final ItemService itemService;
+    private final OrderService orderService;
 
     @GetMapping(value = {"/admin/v2/items", "/admin/v2/items/{page}"})
     public Page<Item> adminItemPage(ItemSearchDto itemSearchDto,
@@ -27,5 +30,14 @@ public class ItemControllerTest {
         Pageable pageable = PageRequest.of(page.orElse(0),3);
 
         return itemService.getAdminItemPage(itemSearchDto, pageable);
+    }
+
+    @GetMapping(value = {"/admin/v2/orders", "/admin/v2/orders/{page}"})
+    public Page<OrderHistoryDto> orderHistoryList(@PathVariable("page") Optional<Integer> page,
+                                                  Principal principal) {
+
+        Pageable pageable = PageRequest.of(page.orElse(0),3);
+
+        return orderService.getOrderList(principal.getName(), pageable);
     }
 }
